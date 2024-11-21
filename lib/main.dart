0c756332a3ee01185cf2e7ddc99dd77770e714b1
samuel <samuelhutagalung00@gmail.com>
2024-11-21 22:33:31 +0700

    fix: remove example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'routes/appRoutes.dart';
-import 'screens/default.dart';
 import 'widgets/bookTabs.dart';
 import 'package:google_fonts/google_fonts.dart';
 
